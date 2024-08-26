987cec390783e187a1e2f1e81a8e8e5d2254db5e
Diana Grayden <graydendi@mail.ru>
2024-08-26 15:22:13 +0300

    fff

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.curentGuessing = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.curentGuessing = Math.floor((this.min + this.max) / 2);
+        return this.curentGuessing;
     }
 
     lower() {
-
+        this.max = this.curentGuessing - 1;
     }
 
     greater() {
-
+        this.min = this.curentGuessing +1;
     }
 }
 
